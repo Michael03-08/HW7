f97418dfe183bb5db018a6635fc4484590753b36
Flex <cma491628@gmail.com>
2023-10-12 17:30:19 +0300

    реалезация цикла if

--- Main.java
+++ Main.java
@@ -6,13 +6,21 @@ public class Main {
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        boolean isCanCook = false;
 
         Scanner scanner = new Scanner(System.in);
         //todo реализовать ввод пользователем кол-ва ингредиентов.
-        boolean inContinium = true;
-            int input = scanner.nextInt();
-            System.out.print("Введите колличество ингридиентов: ");
-               toadEyesCount = setText("Жабьи глаза:" + input);
+        System.out.print("Ввеите колличество Жабьих глаз: ");
+        toadEyesCount  = scanner.nextInt();
+
+        System.out.print("Введите колличество Слез вурдалака: ");
+        ghoulTearsCount = scanner.nextInt();
+
+        System.out.print("Введите колличество Костей вороно: ");
+        ravenBonesCount = scanner.nextInt();
+
+        System.out.print("Введите колличество Пельмений: ");
+        dumplingsCount = scanner.nextInt();
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -21,5 +29,27 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+
+        if(toadEyesCount >= 3 && toadEyesCount >= 1){
+            System.out.println("Вы сделали Элесир зоркости");
+            isCanCook = true;
+        }
+        if (toadEyesCount >= 2 && dumplingsCount >= 4){
+            System.out.println("Вы сделали Элексир стойкости");
+            isCanCook = true;
+
+        }
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2){
+            System.out.println("Вы сделали Элексир срытности");
+            isCanCook = true;
+
+        }
+        if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3){
+            System.out.println("Вы ссделали Запретныей элексир");
+            isCanCook = true;
+        }
+        if (isCanCook == false){
+            System.out.println("Вы не можете ничего создать");
+        }
     }
 }
